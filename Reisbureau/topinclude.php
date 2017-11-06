78a20f0f495269e97df1ab06f5272946e1a1a19e
koelkast01 <jurgenkatoen@gmail.com>
2017-11-06 14:31:01 +0100

    verschillende rollen

--- Reisbureau/topinclude.php
+++ Reisbureau/topinclude.php
@@ -29,14 +29,33 @@ and open the template in the editor.
             </div>
             <div class="nav">
                 <?php
+                if (isset($_SESSION['Email'])){
+                $email = $_SESSION['Email'];
+                $usertype = "SELECT * FROM gebruikers WHERE Email='$email'";
+                $Rol = mysqli_query($DBConnect,$usertype);
+                $row = mysqli_fetch_assoc($Rol);
+                if ($row["Rol"] == 'Admin')
+                {
+                echo "<br><ul>";
+                echo "<li class='ok'><a href='logout.php' class='are'>Uitloggen </a> </li>";    
+                
+                echo "<li><a href='index.php' class='are'>Home</a></li>";
+                echo "<li><a href='stedentrips.php' class='are'>Stedentrips</a></li>";
+                echo "<li><a href='about.php' class='are'>About</a></li></ul>";
+                }else if($row["Rol"] == 'gebruiker')
+                {
+                 echo "gebruiker";
+                }
+                }
+                else{
                 echo "<br><ul>";
                 echo "<li class='ok'><a href='signup.php' class='are'>Registreren </a></li>";
-                echo "<li class='ok'><a href='logout.php' class='are'>Uitloggen </a> </li>";
                 echo "<li class='ok'><a href='login.php' class='are'>Inloggen </a> </li>";
                 
                 echo "<li><a href='index.php' class='are'>Home</a></li>";
                 echo "<li><a href='stedentrips.php' class='are'>Stedentrips</a></li>";
                 echo "<li><a href='about.php' class='are'>About</a></li></ul>";
+                }
                 ?>
             </div>
 
