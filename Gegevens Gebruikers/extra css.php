45b57dd27096e83350988dc121827a9549fff5d5
jojon11 <33319144+jojon11@users.noreply.github.com>
2017-11-08 12:40:44 +0100

    Add files via upload

---
+++ Gegevens Gebruikers/extra css.php
@@ -0,0 +1,31 @@
+table, td {
+    border-collapse: collapse;
+    border-spacing: 4px;
+    border-color: black;
+
+  }
+
+table {
+      padding-left: 10px;
+}
+  th, td {
+      border-bottom: 1px solid #ddd;
+  }
+
+tr:nth-child(odd) {background-color: #cccccc}
+
+td {
+    height: 30px;
+    padding: 10px;
+}
+
+th {
+    background-color: #808080;
+    color: black;
+    text-align: left;
+}
+
+h2 {
+  padding: inherit;
+  padding-left: 100px;
+}
---
+++ Gegevens Gebruikers/gegevensgebruikers.php
@@ -0,0 +1,60 @@
+<?php include "topinclude.php"; ?>
+<div class="inhoud">
+
+    <h2> Gegevens Gebruikers </h2>
+    <TABLE BORDER = "3">
+    <TR>
+    <TH>Voornaam</TH><TH>Achternaam</TH><TH>E-mail Adres</TH><TH>Geboortedatum</TH><TH>Plaats</TH><TH>Postcode</TH><TH>Straat & Huisnummer</TH><TH>Wachtwoord</TH>
+    </TR>
+    </form>
+</div>
+
+<?php
+
+//assign the connection and selected database to a variable
+     $DBConnect = mysqli_connect("127.0.0.1", "root", "", "reisbureau");
+     if ($DBConnect === FALSE)
+     {
+         echo "<p>Unable to connect to the database server.</p>"
+         . "<p>Error code " . mysqli_errno() . ": "
+         . mysqli_error() . "</p>";
+     }
+
+$rs = mysqli_query($DBConnect,"SELECT * FROM gebruikers");
+
+?>
+
+    <?php
+    if( $rs){
+
+        while($row = mysqli_fetch_assoc($rs)){
+          echo "<TR>";
+          echo "<TD>" . $row['Voornaam'] . "</TD>";
+          echo "<TD>" . $row['Achternaam'] . "</TD>";
+          echo "<TD>" . $row['Email'] . "</TD>";
+          echo "<TD>" . $row['Geboortedatum'] . "</TD>";
+          echo "<TD>" . $row['Woonplaats'] . "</TD>";
+          echo "<TD>" . $row['Postcode'] . "</TD>";
+          echo "<TD>" . $row['Staat_huisnummer'] . "</TD>";
+          echo "<TD>" . $row['Wachtwoord'] . "</TD>";
+          echo "</TR>";
+
+            }
+        echo "</TABLE>";
+
+
+
+
+
+
+    }else{
+      echo '<p>';
+      echo mysqli_error($db);
+      echo '</p>';
+    }
+     ?>
+  </div>
+
+<?php include "botinclude.php";
+  mysqli_close($DBConnect)
+ ?>
